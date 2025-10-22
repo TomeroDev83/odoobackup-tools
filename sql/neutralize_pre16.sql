feb5219f62ca3fd3ca0f0ec3632134e8c2868935
TomeroDev83 <a.tomeroherrero@gmail.com>
2025-10-22 16:16:28 +0200

    Add new License

--- sql/neutralize_pre16.sql
+++ sql/neutralize_pre16.sql
@@ -1,3 +1,7 @@
+-- Copyright 2025 TomeroDev83
+-- Licensed under the Apache License, Version 2.0
+-- http://www.apache.org/licenses/LICENSE-2.0
+
 -- Deactivate crons
 UPDATE ir_cron SET active=FALSE;
 
